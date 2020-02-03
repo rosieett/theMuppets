072e3bb0726d15f49079cff847441ffab6752ab5
Rosie Ettenheim <rosie.ett@gmail.com>
2020-02-03 10:07:51 -0500

    css updates

---
+++ js/app2.js
@@ -0,0 +1,55 @@
+//doc ready
+
+//each muppet should have a class that says which type and appeared on
+var sesameStreet = '#sesameStreet';
+
+//upon click of button: Animal
+
+//jquery finds muppet grid-item that has Animal
+
+//jquery shows that muppet in grid area
+
+
+
+
+
+
+// //document ready
+// $(document).ready(function() {
+//
+//   //prevent the submit button from refreshing the page
+//   $('.button').click(function(e){
+//   event.preventDefault(e);
+//
+//     //get the value of the #animal input
+//     var animal = $('#animal').val();
+//     $('#animal').val();
+//
+//     //get the value of the #people input
+//     var people = $('#people').val();
+//     $('#people').val();
+//
+//     //get the value of the #monster input
+//     var monster = $('#monster').val();
+//
+//
+//     //get the value of the #sesameStreet input
+//     var sesameStreet = $('#sesameStreet').val();
+//     $('#sesameStreet').val();
+//
+//     //get the value of the #theMuppetShow input
+//     var theMuppetShow = $('#theMuppetShow').val();
+//     $('#theMuppetShow').val();
+//
+//       // if the user clicks people, var people should Show
+//       if (monster == '#monster') {
+//         $('#monster').val();
+//       }
+//
+//
+//       else if (people == '#people') {
+//         $('people').attr('data-type','animal');
+//       }
+//       });
+//
+// });
