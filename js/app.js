528ec7fb048e28c810568cd37fa350d193b5e5cf
Rosie Ettenheim <rosie.ett@gmail.com>
2020-02-06 16:06:53 -0500

    buttons and headers

--- js/app.js
+++ js/app.js
@@ -1,45 +1,155 @@
-//create a click function
-$('button').on('click', function(){
+// Set the default variables for what's being shown
+var showType = "all";
+var muppetType = "all"
 
-  //get the data-category attr
-  var dataType = $(this).attr('data-type');
+//-------DOCUMENT READY FUNCTION-------
 
-  //get the data-show attr
-  var dataShow = $(this).attr('data-show');
+//When the page is loaded, run the javascript
+$(document).ready(function() {
+
+  //SHOWING MUPPETS IN THE GRID without imgs being in the html
+  $(muppetData).each(function(key,value){
+
+    //jquery append - inserts inside the target
+    var newItem = '<div class="grid-item" data-muppettype="';
+    newItem = newItem + value.category + '" ' +  'data-showtype="' + value.show + '">' ;
+    newItem = newItem + '<img src="images/muppetImages/'+value.filename+'.png" class="muppetImg" alt="'+value.muppet+'">';
+    newItem = newItem + '</div>';
+
+    $('.grid').append(newItem)
+
+
+    console.log(value.muppet);
+
+  });
+
+  //----TOGGLED BUTTON COLOR CHANGE-----
+
+      //function to make the button color change when in active state
+
+  //---ShowType---
+
+  //When I click on the filter buttons, run this function
+  $('#showType .button').click(function(e){
+
+    //prevent the submit button from refreshing the page
+    e.preventDefault();
+
+    //Get the Show - this lets you click on any button in this area and only need to write once
+    showType = $(this).attr("id");
+
+    //output these values to console.
+    console.log("Showing muppets of type " + muppetType + " that appears on " + showType);
+
+    muppetFilter();
+  });
+
+  //---MuppetType---
+
+  //When I click on the filter buttons, run this function
+  $('#muppetType .button').click(function(e){
+
+      //prevent the submit button from refreshing the page
+      e.preventDefault();
+
+      //Get the Show - this lets you click on any button in this area and only need to write once
+      muppetType = $(this).attr("id");
+
+      //output these values to console.
+      console.log("Showing muppets of type " + muppetType + " that appears on " + showType);
+
+      muppetFilter();
+  });
+
+  //When I click on the filter buttons, run this function
+  $('#reset .button').click(function(e){
+
+      //prevent the submit button from refreshing the page
+      e.preventDefault();
+
+      //Get the Show - this lets you click on any button in this area and only need to write once
+      muppetType = "all";
+      showType = "all";
+
+      //output these values to console.
+      console.log("Showing muppets of type " + muppetType + " that appears on " + showType);
+
+      muppetFilter();
+  });
+
+
+  // --------ISOTOPE FILTERING STYLES--------
+
+    // init Isotope
+    $('.grid').isotope({
+      // options
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows'
+    });
+
+    // init Isotope
+    var $grid = $('.grid').isotope({
+      // options
+    });
+
+    // filter items on button click
+    $('.button').click(function() {
+      var filterValue = $(this).attr('muppetData');
+      $grid.isotope({ filter: filterValue });
+    });
+
+    //---- Combination filters --------
+
+    // store filter for each group
+      var filters = {};
+
+      $demo.click(function() {
+        var $this = $(this);
+        // get group key
+        var $buttonGroup = $this.parents('.button-group');
+        var filterGroup = $buttonGroup.attr('data-filter-group');
+        // set filter for group
+        filters[ filterGroup ] = $this.attr('data-filter');
+        // combine filters
+        var filterValue = concatValues( filters );
+        $grid.isotope({ filter: filterValue });
+      });
+
+      // flatten object by concatting values
+      function concatValues( obj ) {
+        var value = '';
+        for ( var prop in obj ) {
+          value += obj[ prop ];
+        }
+        return value;
+      }
+
 
 });
 
-// init Isotope
-var $grid = $('.grid').isotope({
-  // options
-});
 
-//Initialize with jQuery
-$('.grid').isotope({
-  // options
-  itemSelector: '.grid-item' ,
-  layoutMode: 'fitRows'
-});
+//Overall Function
+function muppetFilter (){
+  //hide everyone
+  $('.grid-item').hide();
 
-// filter .animal items
-$grid.isotope({ filter: '.animal' });
+  //show just the ones I want
+  jQuery(".grid .grid-item").each(
+              //loop to find each muppet div
+              function(key,value){
 
-// filter .person
-$grid.isotope({ filter: '.person' });
+                //console.log to text
+                // console.log($(this).data('showtype'));
 
-// filter .sesameStreet
-$grid.isotope({ filter: '.sesameStreet' });
+              var itemShowType = jQuery(this).data("showtype");
+              var itemMuppetType = jQuery(this).data("muppettype");
 
-// filter .theMuppetShow
-$grid.isotope({ filter: '.theMuppetShow' });
-
-// show all items
-$grid.isotope({ filter: '*' });
-
-
-
-// filter items on button click
-$('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});
+              if(itemShowType == showType || showType == "all"){
+                  // The show matches
+                  if(itemMuppetType == muppetType || muppetType == "all"){
+                      jQuery(this).show();
+                  }
+              }
+          }
+      );
+};
