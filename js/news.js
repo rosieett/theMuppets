a03eed41842ea2e4756e8636228ce698776bb2c7
rosieett <rosie.ett@gmail.com>
2021-02-09 21:22:22 -0500

    news update

--- js/news.js
+++ js/news.js
@@ -1,78 +1,27 @@
-
-
-
-
-//create function to retrieve info from NYTimes
-// const muppetsNewYorkTimes = async () => {
-//
-//   //Retrieve information from the URl
-//   const muppetsNytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=(’muppets’)&begin_date=20200101&api-key=2cgcwOVTzV8ysIeRYSecL4dSRdAwI9Rd';
-//   // create a variable to go get my url and store it
-//   const muppetResponse = await fetch(muppetsNytimesUrl);
-//   // if it's not ok, let's get out of here
-//   if (!muppetResponse.ok) {
-//     throw new Error('failed to retrieve muppets from nytimes');
-//   } else if (!sesameResponse.ok) {
-//     throw new Error('failed to retrieve sesamest from nytimes');
-//   }
-//
-//   // transform response into JSON
-//   const jsonMuppetsNyt = await muppetResponse.json();
-//   let muppetInfo = jsonMuppetsNyt.response.docs;
-//
-//   let rowContainer = document.querySelector(".row");
-//
-//   for (let i = 0; i < muppetInfo.length; i++) {
-//     let obj = muppetInfo[i];
-//     let col = document.createElement('div');
-//         col.className = "col-4 mb-4";
-//     let card = document.createElement('div');
-//         card.className = "card";
-//         card.style = "width: 18rem;";
-//     let cardBody = document.createElement('div');
-//         cardBody.className = "card-body";
-//     let imgLink = `http://www.nytimes.com/` + obj.multimedia[0].url;
-//     let embed = document.createElement('div');
-//         embed.className = "embed-responsive embed-responsive-4by3 card-image-top";
-//     let embedR = document.createElement('div');
-//         embedR.className = "embed-responsive-item articleImage"
-//         embedR.style.backgroundImage = "url('" + imgLink + "')";
-//     let title = document.createTextNode(obj.headline.main);
-//     let h6 = document.createElement('h6');
-//         h6.className = "card-title";
-//     let anchor = document.createElement("a");
-//         anchor.className = "btn btn-primary";
-//         anchor.target = "_blank";
-//         anchor.innerText = "Go to Article"
-//         anchor.href = obj.web_url;
-//
-//
-//   	h6.appendChild(title);
-//     cardBody.appendChild(h6);
-//     cardBody.appendChild(anchor);
-//     card.appendChild(embed);
-//     embed.appendChild(embedR);
-//     card.appendChild(cardBody);
-//     col.appendChild(card)
-//   	rowContainer.appendChild(col);
-//   }
-// }
-
 import { nyTimesKey } from "./keys.js";
 
-const sesamestNewYorkTimes = async () => {
+create function to retrieve info from NYTimes
+const muppetsNewYorkTimes = async () => {
 
-  const sesamestNytimesUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=(’sesame street’)&begin_date=20200101&pub_date&api-key=${nyTimesKey}`;
-  const sesameResponse = await fetch(sesamestNytimesUrl);
+  //Retrieve information from the URl
+  const muppetsNytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=(’muppets’)&begin_date=20200101&api-key=2cgcwOVTzV8ysIeRYSecL4dSRdAwI9Rd';
+  // create a variable to go get my url and store it
+  const muppetResponse = await fetch(muppetsNytimesUrl);
+  // if it's not ok, let's get out of here
+  if (!muppetResponse.ok) {
+    throw new Error('failed to retrieve muppets from nytimes');
+  } else if (!sesameResponse.ok) {
+    throw new Error('failed to retrieve sesamest from nytimes');
+  }
 
   // transform response into JSON
-  const jsonSesameStNyt = await sesameResponse.json();
-  let sesameStInfo = jsonSesameStNyt.response.docs;
+  const jsonMuppetsNyt = await muppetResponse.json();
+  let muppetInfo = jsonMuppetsNyt.response.docs;
 
   let rowContainer = document.querySelector(".row");
 
-  for (let i = 0; i < sesameStInfo.length; i++) {
-    let obj = sesameStInfo[i];
+  for (let i = 0; i < muppetInfo.length; i++) {
+    let obj = muppetInfo[i];
     let col = document.createElement('div');
         col.className = "col-4 mb-4";
     let card = document.createElement('div');
@@ -94,21 +43,10 @@ const sesamestNewYorkTimes = async () => {
         anchor.target = "_blank";
         anchor.innerText = "Go to Article"
         anchor.href = obj.web_url;
-    let summary = document.createTextNode(obj.abstract);
-    let abstract = document.createElement('p');
-        abstract.className = "card-text";
-    let pubDate = obj.pub_date;
-        pubDate = pubDate.slice(0,10);
-    let pubDateBetter = document.createTextNode(pubDate);
-    let published = document.createElement('p');
-        published.className = "card-text about mb-0 mt-0";
+
 
   	h6.appendChild(title);
-    published.appendChild(pubDateBetter);
-    cardBody.appendChild(published);
-    abstract.appendChild(summary);
     cardBody.appendChild(h6);
-    cardBody.appendChild(abstract);
     cardBody.appendChild(anchor);
     card.appendChild(embed);
     embed.appendChild(embedR);
@@ -119,6 +57,66 @@ const sesamestNewYorkTimes = async () => {
 }
 
 
+//
+// const sesamestNewYorkTimes = async () => {
+//
+//   const sesamestNytimesUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=(’sesame street’)&begin_date=20200101&pub_date&api-key=${nyTimesKey}`;
+//   const sesameResponse = await fetch(sesamestNytimesUrl);
+//
+//   // transform response into JSON
+//   const jsonSesameStNyt = await sesameResponse.json();
+//   let sesameStInfo = jsonSesameStNyt.response.docs;
+//
+//   let rowContainer = document.querySelector(".row");
+//
+//   for (let i = 0; i < sesameStInfo.length; i++) {
+//     let obj = sesameStInfo[i];
+//     let col = document.createElement('div');
+//         col.className = "col-4 mb-4";
+//     let card = document.createElement('div');
+//         card.className = "card";
+//         card.style = "width: 18rem;";
+//     let cardBody = document.createElement('div');
+//         cardBody.className = "card-body";
+//     let imgLink = `http://www.nytimes.com/` + obj.multimedia[0].url;
+//     let embed = document.createElement('div');
+//         embed.className = "embed-responsive embed-responsive-4by3 card-image-top";
+//     let embedR = document.createElement('div');
+//         embedR.className = "embed-responsive-item articleImage"
+//         embedR.style.backgroundImage = "url('" + imgLink + "')";
+//     let title = document.createTextNode(obj.headline.main);
+//     let h6 = document.createElement('h6');
+//         h6.className = "card-title";
+//     let anchor = document.createElement("a");
+//         anchor.className = "btn btn-primary";
+//         anchor.target = "_blank";
+//         anchor.innerText = "Go to Article"
+//         anchor.href = obj.web_url;
+//     let summary = document.createTextNode(obj.abstract);
+//     let abstract = document.createElement('p');
+//         abstract.className = "card-text";
+//     let pubDate = obj.pub_date;
+//         pubDate = pubDate.slice(0,10);
+//     let pubDateBetter = document.createTextNode(pubDate);
+//     let published = document.createElement('p');
+//         published.className = "card-text about mb-0 mt-0";
+//
+//   	h6.appendChild(title);
+//     published.appendChild(pubDateBetter);
+//     cardBody.appendChild(published);
+//     abstract.appendChild(summary);
+//     cardBody.appendChild(h6);
+//     cardBody.appendChild(abstract);
+//     cardBody.appendChild(anchor);
+//     card.appendChild(embed);
+//     embed.appendChild(embedR);
+//     card.appendChild(cardBody);
+//     col.appendChild(card)
+//   	rowContainer.appendChild(col);
+//   }
+// }
+//
+//
 
 
 // DOM is ready for manipulation
@@ -129,8 +127,8 @@ const onLoadHandler = () => {
   } catch (err) {
     console.log("yeah this isn't working...")
   }
-  // muppetsNewYorkTimes();
-  sesamestNewYorkTimes();
+  muppetsNewYorkTimes();
+  // sesamestNewYorkTimes();
 
 };
 
