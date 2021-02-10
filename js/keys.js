fc1e2f9ee31b6ec46a7393c08b0b743f6c943ef0
rosieett <rosie.ett@gmail.com>
2021-02-09 21:16:06 -0500

    news update

--- js/keys.js
+++ js/keys.js
@@ -1,4 +1,4 @@
 
 // const guardianKey = '036a5f2b-cda3-4792-9cad-fd8a5e1cc390';
-export const nyTimesKey = '2cgcwOVTzV8ysIeRYSecL4dSRdAwI9Rd';
+const nyTimesKey = '2cgcwOVTzV8ysIeRYSecL4dSRdAwI9Rd';
 // const newsApiKey = 'eb2d1ced8c9149bb9a14349bd2cbe517';
